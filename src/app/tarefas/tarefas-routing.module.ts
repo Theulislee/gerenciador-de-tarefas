2d54f7be968800918d7b36388d849d494315616e
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-12-07 16:05:17 -0300

    add comment sobre mapeamento

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes } from '@angular/router';
 import { ListarTarefaComponent } from './listar';
 
 export const TarefaRoutes: Routes =[
-  {
+
+  { //atalho para tarefas/listar, e em seguida irá redirecionar para tarefas/listar
   path: 'tarefas',
   redirectTo: 'tarefas/listar'
 },
 
-{
+{ //primeira rota principal, quando digitar tarefas/listar exibirá conteúdo de tarefaComponent
   path: 'tarefas/listar',
   component: ListarTarefaComponent
 }
