8ff6176e6a84dd288240bce597442db7c638aa35
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-12-11 09:41:44 -0300

    add comments tarefas.module.ts

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TarefasService } from './shared'; // add import de TarefaSService
 import { ListarTarefaComponent } from './listar'; //add import de TarefasService //atualizando: apagar o restante deixando listar
 import { RouterModule } from '@angular/router'; //add import do RouterModule para o Angular reconhecer
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; //add import do FormsModule para o Angular reconhecer
 import { CadastrarTarefaComponent } from './cadastrar'; //add import do FormsModule para o Angular reconhecer //reduzido para cadastrar
 
 
@@ -14,8 +14,8 @@ import { CadastrarTarefaComponent } from './cadastrar'; //add import do FormsMod
   ],
   imports: [
     CommonModule,
-    RouterModule,
-    FormsModule
+    RouterModule, //add RouterModule do import
+    FormsModule //add FormsModule do import
   ],
   providers: [
     TarefasService // add import de TarefasService
