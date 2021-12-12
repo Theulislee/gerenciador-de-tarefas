651e32cb28a28eb17212463d3a2ef410eff8ce1b
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-12-12 09:43:10 -0300

    add import and atribute tarefa on cadastrar-tarefa.ts

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TarefasService, Tarefa } from '../shared'; //add import para validação do tarefa: Tarefa
 
 @Component({
   selector: 'app-cadastrar-tarefa',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar-tarefa.component.css']
 })
 export class CadastrarTarefaComponent implements OnInit {
+
+  tarefa: Tarefa
 
   constructor() { }
 
