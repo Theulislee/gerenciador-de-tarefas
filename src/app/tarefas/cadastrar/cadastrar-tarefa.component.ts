eeedbb1c73366437562951807f742557460151ab
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-12-12 10:42:37 -0300

    add import NgForm to cadastrar-tarefa

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core'; //add viewChild
-import { NgForm } from '@angular/forms';
+import { NgForm } from '@angular/forms'; //add NgForm que está add na class
 import { TarefasService, Tarefa } from '../shared'; //add import para validação do tarefa: Tarefa
+
 
 @Component({
   selector: 'app-cadastrar-tarefa',
@@ -10,6 +11,7 @@ import { TarefasService, Tarefa } from '../shared'; //add import para validaçã
 export class CadastrarTarefaComponent implements OnInit {
 
 //add validacao do angular que vai permitir referência de formulário html
+//NgForm é a classe padrão do angular que deve ser importada
   @ViewChild('formTarefa') formTarefa: NgForm;
 
   tarefa: Tarefa //add para associar campo de texto da interface html
